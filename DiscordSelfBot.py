11050375412d5c40308a222623b4563ccdd1ab8c
DARKSONIC <kevin-lehmann99@web.de>
2023-12-23 23:34:12 +0100

    Changed work() function, created error statements. As of 23.12.2023, self bots seem to be against the TOS, so there won't be any updates on this project anytime soon.

--- DiscordSelfBot.py
+++ DiscordSelfBot.py
@@ -1,17 +1,36 @@
 import requests
-import time
 import os
 from threading import Thread
 from dotenv import load_dotenv
 
 load_dotenv()
-DISCORD_AUTHORIZATION_TOKEN = os.getenv('DISCORD_AUTHORIZATION_TOKEN')  # Never give this away, reset password to change
-SERVER_IP = os.getenv('SERVER_IP')  # Change server through channel link
+
+DISCORD_AUTHORIZATION_TOKEN = os.getenv('DISCORD_AUTHORIZATION_TOKEN')
+SERVER_IP = os.getenv('SERVER_IP')
 
 working = {
-    'content': "!rp work"
+    'content': "Hallo"
 }
 
 header = {
     'authorization': DISCORD_AUTHORIZATION_TOKEN
 }
+
+
+def work():
+    try:
+        response = requests.post(SERVER_IP, data=working, headers=header)
+        if response.status_code == 200:
+            print("Message sent!")
+        else:
+            print("Error sending Message:", response.status_code)
+    except Exception as error:
+        print("Error trying to execute Code:", str(error))
+
+
+thread_1 = Thread(target=work)
+
+try:
+    thread_1.start()
+except Exception as e:
+    print("Error starting Threads:", str(e))
